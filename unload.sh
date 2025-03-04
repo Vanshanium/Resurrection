40a7d6a00eacc1716bdb9d8c046ad02e5bb9b5ff
Voob <vansh.singh8272@gmail.com>
2025-03-04 17:16:08 +0530

    Theme is automated

--- unload.sh
+++ unload.sh
@@ -170,6 +170,15 @@ sudo apt install zsh
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # This is the extension for oh my zsh 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    # Downloading and setting oh my posh
 curl -s https://ohmyposh.dev/install.sh | bash -s 
+mkdir ~/.poshthemes/
+mv ./Setup_files/creeper.json ~/.poshthemes/
+
+rm ~/.zshrc 
+mv ./Setup_files/.zshrc 
+zsh
 
