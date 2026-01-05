bbd993d311a17b578d3cbac722888805e24e808b
vanshanium <vansh.singh8272@gmail.com>
2026-01-05 19:50:26 +0530

    Add : New Hyprland and Hyprpaper configs

--- Linux/Setup_files/Dotfiles/nvim/lua/plugins/treesitter.lua
+++ Linux/Setup_files/Dotfiles/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "c", "cpp", "javascript", "typescript", "html", "css" },
+        ensure_installed = { "lua", "python", "c", "cpp", "javascript", "typescript", "html", "css","r" },
         highlight = {
           enable = true,
         },
